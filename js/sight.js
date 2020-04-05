e46cd595f5c15c01a5b47b9f215b3f8a0f1b1acd
Siva Challa <sivachalla@yahoo.com>
2020-04-04 22:34:17 -0700

    Completed module

---
+++ js/sight.js
@@ -0,0 +1,35 @@
+class SVGElement {
+	constructor(type) {
+		this.type = type;
+		this.namespace = 'http://www.w3.org/2000/svg';
+		this.node = document.createElementNS(this.namespace, this.type);
+		return this;
+	}
+	
+	attr(attrs) {
+		console.log('Begin: attr() method');
+		for (const [key, value] of attrs.entries()) {
+			console.log(key, value);
+			this.node.setAttributeNS(null, key, value);
+		}
+		console.log('End: attr() method');
+		return this;
+	}
+	
+	append(element) {
+		const parent =  (typeof element === 'string') ? 
+			document.querySelector(element) : element.node;
+		parent.appendChild(this.node);
+	}
+}
+
+class Sight {
+	constructor(selector, width, height) {
+		this.svg = new SVGAElement('svg').attr({viewbox: `0 0 ${width} ${height}`}).append(selector);
+	}
+
+	draw(type, attrs) {
+		return new SVGElement(type).attrs(attrs).append(this.svg);
+	}
+}
+
